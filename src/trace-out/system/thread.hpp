890d9f7317d9c26c0cc596dd8439693d4339414a
shrpnsld <anton.degtiar@gmail.com>
2023-09-20 23:59:51 +0300

    Moved system thread to a single header

--- src/trace-out/system/thread.hpp
+++ src/trace-out/system/thread.hpp
@@ -1,14 +1,51 @@
 #pragma once
 
 #include "trace-out/standard/integer.hpp"
+#include "trace-out/stuff/platform-detection.hpp"
 
 
 namespace trace_out { namespace detail { namespace system
 {
 
-	standard::uint64_t current_thread_id();
+	inline standard::uint64_t current_thread_id();
 
 }
 }
 }
+
+#if defined(TRACE_OUT_POSIX)
+
+#include <pthread.h> // [amalgamate:leave]
+
+
+namespace trace_out { namespace detail { namespace system
+{
+
+	standard::uint64_t current_thread_id()
+	{
+		return reinterpret_cast<standard::uint64_t>(pthread_self());
+	}
+
+}
+}
+}
+
+#elif defined(TRACE_OUT_WINDOWS)
+
+#include <windows.h> // [amalgamate:leave]
+
+
+namespace trace_out { namespace detail { namespace system
+{
+
+	standard::uint64_t current_thread_id()
+	{
+		return static_cast<standard::uint64_t>(GetCurrentThreadId());
+	}
+
+}
+}
+}
+
+#endif
 
