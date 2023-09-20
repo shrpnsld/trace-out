68d0f2f5f210b0671ac223122d7f9a31cf6d5e73
shrpnsld <anton.degtiar@gmail.com>
2023-09-20 23:35:02 +0300

    Moved printfamily functions to a single header

--- src/trace-out/standard/printfamily.hpp
+++ src/trace-out/standard/printfamily.hpp
@@ -1,16 +1,62 @@
 #pragma once
 
 #include <cstdarg>
+#include <cstdio>
 
 #include "trace-out/standard/integer.hpp"
+#include "trace-out/stuff/platform-detection.hpp"
 
 
 namespace trace_out { namespace detail { namespace standard
 {
 
-	size_t vsnprintf_string_length(const char *format, va_list arguments);
-	int vsnprintf(char *buffer, size_t size, const char *format, va_list arguments);
+	inline size_t vsnprintf_string_length(const char *format, va_list arguments);
+	inline int vsnprintf(char *buffer, size_t size, const char *format, va_list arguments);
 
 }
 }
 }
+
+#if defined(TRACE_OUT_CLANG) || defined(TRACE_OUT_GCC) || defined(TRACE_OUT_MINGW)
+
+namespace trace_out { namespace detail { namespace standard
+{
+
+	size_t vsnprintf_string_length(const char *format, va_list arguments)
+	{
+		int retval = ::vsnprintf(NULL, 0, format, arguments);
+		return static_cast<size_t>(retval);
+	}
+
+
+	int vsnprintf(char *buffer, size_t size, const char *format, va_list arguments)
+	{
+		return ::vsnprintf(buffer, size, format, arguments);
+	}
+
+}
+}
+}
+
+#elif defined(TRACE_OUT_MVS)
+
+namespace trace_out { namespace detail { namespace standard
+{
+
+	size_t vsnprintf_string_length(const char *format, va_list arguments)
+	{
+		int retval = _vscprintf(format, arguments);
+		return static_cast<size_t>(retval);
+	}
+
+
+	int vsnprintf(char *buffer, size_t size, const char *format, va_list arguments)
+	{
+		return _vsnprintf_s(buffer, size, _TRUNCATE, format, arguments);
+	}
+
+}
+}
+}
+
+#endif
