e552867de577d71b543348c1c14a68b8ba059a40
shrpnsld <anton.degtiar@gmail.com>
2023-09-26 23:38:37 +0300

    Added 'TRACE_OUT_TO_NETWORK' manual test helper

---
+++ tests/to-network-option/to-network.tests.cpp
@@ -0,0 +1,8 @@
+#include "trace-out/trace-out.hpp"
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("'TRACE_OUT_TO_NETWORK' manual test helper", "[TRACE_OUT_TO_NETWORK]")
+{$f
+	$p("hellomoto!")
+}
+
