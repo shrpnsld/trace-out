f847cac5019479ebce4f18946f6019007ea3a77b
shrpnsld <anton.degtiar@gmail.com>
2023-10-04 22:46:34 +0300

    Corrections and cosmetics

--- tests/stream-to-file-option/stream-to-file-option.tests.cpp
+++ tests/stream-to-file-option/stream-to-file-option.tests.cpp
@@ -15,54 +15,56 @@ TEST_CASE("'TRACE_OUT_STREAM_TO_FILE' option", "[TRACE_OUT_STREAM_TO_FILE]")
 	subject_func();
 
 	std::ifstream file(TRACE_OUT_STREAM_TO_FILE);
+	REQUIRE(file.is_open());
+
 	std::stringstream expected;
 	expected
 		<< R"(@@ ~~~~\[Thread: [0-9a-f]+ main\]~~~~)" << std::endl
-		<< R"(@@  stream-to-file-opt~:81   \| int subject_func\(\))" << std::endl
+		<< R"(@@  stream-to-file-opt~:83   \| int subject_func\(\))" << std::endl
 		<< R"(@@                           \| \{)" << std::endl
-		<< R"(@@  stream-to-file-opt~:86   \|   // hellomoto!)" << std::endl
-		<< R"(@@  stream-to-file-opt~:87   \|   subject1 = 456)" << std::endl
-		<< R"(@@  stream-to-file-opt~:88   \|   subject2 = 789.123)" << std::endl
-		<< R"(@@  stream-to-file-opt~:89   \|   subject3 = "hellomoto!")" << std::endl
-		<< R"(@@  stream-to-file-opt~:90   \|   \[subject3\.begin\(\), subject3\.end\(\)\) = \['h', 'e', 'l', 'l', 'o', 'm', 'o', 't', 'o', '!'\])" << std::endl
-		<< R"(@@  stream-to-file-opt~:91   \|   subject3.c_str\(\), 10 bytes of 1-byte hexadecimal)" << std::endl
+		<< R"(@@  stream-to-file-opt~:88   \|   // hellomoto!)" << std::endl
+		<< R"(@@  stream-to-file-opt~:89   \|   subject1 = 456)" << std::endl
+		<< R"(@@  stream-to-file-opt~:90   \|   subject2 = 789.123)" << std::endl
+		<< R"(@@  stream-to-file-opt~:91   \|   subject3 = "hellomoto!")" << std::endl
+		<< R"(@@  stream-to-file-opt~:92   \|   \[subject3\.begin\(\), subject3\.end\(\)\) = \['h', 'e', 'l', 'l', 'o', 'm', 'o', 't', 'o', '!'\])" << std::endl
+		<< R"(@@  stream-to-file-opt~:93   \|   subject3.c_str\(\), 10 bytes of 1-byte hexadecimal)" << std::endl
 		<< R"(@@                           \|     [0-9a-f]+: 68 65 6c 6c 6f 6d 6f 74)" << std::endl
 		<< R"(@@                           \|     [0-9a-f]+: 6f 21)" << std::endl
 		<< R"(@@                           \| )" << std::endl
-		<< R"(@@  stream-to-file-opt~:92   \|   subject3 = "wazuuup!"; // running\.\.\.)" << std::endl
-		<< R"(@@  stream-to-file-opt~:92   \|   subject3 = "wazuuup!"; // done\.)" << std::endl
-		<< R"(@@  stream-to-file-opt~:94   \|   if \(true\) => true)" << std::endl
+		<< R"(@@  stream-to-file-opt~:94   \|   subject3 = "wazuuup!"; // running\.\.\.)" << std::endl
+		<< R"(@@  stream-to-file-opt~:94   \|   subject3 = "wazuuup!"; // done\.)" << std::endl
+		<< R"(@@  stream-to-file-opt~:96   \|   if \(true\) => true)" << std::endl
 		<< R"(@@                           \|   \{)" << std::endl
 		<< R"(@@                           \|   \})" << std::endl
 		<< R"(@@                           \| )" << std::endl
-		<< R"(@@  stream-to-file-opt~:98   \|   for \(;;\))" << std::endl
+		<< R"(@@  stream-to-file-opt~:100  \|   for \(;;\))" << std::endl
 		<< R"(@@                           \|   \{)" << std::endl
-		<< R"(@@  stream-to-file-opt~:98   \|     // for: iteration #1)" << std::endl
+		<< R"(@@  stream-to-file-opt~:100  \|     // for: iteration #1)" << std::endl
 		<< R"(@@                           \| )" << std::endl
 		<< R"(@@                           \|   \} // for \(;;\))" << std::endl
 		<< R"(@@                           \| )" << std::endl
-		<< R"(@@  stream-to-file-opt~:103  \|   while \(true\))" << std::endl
+		<< R"(@@  stream-to-file-opt~:105  \|   while \(true\))" << std::endl
 		<< R"(@@                           \|   \{)" << std::endl
-		<< R"(@@  stream-to-file-opt~:103  \|     // while: iteration #1)" << std::endl
+		<< R"(@@  stream-to-file-opt~:105  \|     // while: iteration #1)" << std::endl
 		<< R"(@@                           \| )" << std::endl
 		<< R"(@@                           \|   \} // while \(true\))" << std::endl
 		<< R"(@@                           \| )" << std::endl
-		<< R"(@@  stream-to-file-opt~:108  \|   timing "dummy"\.\.\.)" << std::endl
-		<< R"(@@  stream-to-file-opt~:108  \|   "dummy" timed in [0-9]+ ms)" << std::endl
-		<< R"(@@  stream-to-file-opt~:109  \|   clocking "dummy"\.\.\.)" << std::endl
-		<< R"(@@  stream-to-file-opt~:109  \|   "dummy" clocked in [0-9]+ clocks \([0-9\.]+ ms\))" << std::endl
-		<< R"(@@  stream-to-file-opt~:110  \|   // execution time statistics \(ms\) for "dummy":)" << std::endl
+		<< R"(@@  stream-to-file-opt~:110  \|   timing "dummy"\.\.\.)" << std::endl
+		<< R"(@@  stream-to-file-opt~:110  \|   "dummy" timed in [0-9]+ ms)" << std::endl
+		<< R"(@@  stream-to-file-opt~:111  \|   clocking "dummy"\.\.\.)" << std::endl
+		<< R"(@@  stream-to-file-opt~:111  \|   "dummy" clocked in [0-9]+ clocks \([0-9\.]+ ms\))" << std::endl
+		<< R"(@@  stream-to-file-opt~:112  \|   // execution time statistics \(ms\) for "dummy":)" << std::endl
 		<< R"(@@                           \|   //   avg/med: [0-9]+ / [0-9]+)" << std::endl
 		<< R"(@@                           \|   //      mode: [0-9]+ \(100% of all values\))" << std::endl
 		<< R"(@@                           \|   //     range: [0-9]+ \[[0-9]+\.\.\.[0-9]+\])" << std::endl
 		<< R"(@@                           \| )" << std::endl
-		<< R"(@@  stream-to-file-opt~:111  \|   // execution time statistics \(clocks\) for "dummy":)" << std::endl
+		<< R"(@@  stream-to-file-opt~:113  \|   // execution time statistics \(clocks\) for "dummy":)" << std::endl
 		<< R"(@@                           \|   //   avg/med: [0-9]+ / [0-9]+)" << std::endl
 		<< R"(@@                           \|   //      mode: [0-9]+ \([0-9]+% of all values\))" << std::endl
 		<< R"(@@                           \|   //     range: [0-9]+ \[[0-9]+\.\.\.[0-9]+\])" << std::endl
 		<< R"(@@                           \| )" << std::endl
-		<< R"(@@  stream-to-file-opt~:113  \|   return 789)" << std::endl
-		<< R"(@@  stream-to-file-opt~:81   \| \} // int subject_func\(\))" << std::endl
+		<< R"(@@  stream-to-file-opt~:115  \|   return 789)" << std::endl
+		<< R"(@@  stream-to-file-opt~:83   \| \} // int subject_func\(\))" << std::endl
 		<< R"(@@                           \| )" << std::endl;
 
 	while (!file.eof() && !expected.eof())
