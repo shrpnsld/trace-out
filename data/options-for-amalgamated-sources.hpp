f5015550761a2282cc32851b932794e479b311fd
shrpnsld <anton.degtiar@gmail.com>
2023-10-19 17:40:47 +0300

    Updated 'options-for-amalgamated-sources.hpp'

---
+++ data/options-for-amalgamated-sources.hpp
@@ -0,0 +1,13 @@
+// #define TRACE_OUT_OFF // turn trace-out off
+// #define TRACE_OUT_MARKER "@@" // add text marker before every trace-out line
+// #define TRACE_OUT_SHOW_FILE_LINE // print file and line information
+// #define TRACE_OUT_INDENTATION "  " // indentation for the output
+// #define TRACE_OUT_STRIP_SCOPE 1 // number of scope components to be shown
+// #define TRACE_OUT_STREAM_TO=<namespace> // access stream by using function "std::ostream &<namespace>::stream()"
+// #define TRACE_OUT_STREAM_TO_ENDPOINT "127.0.0.1:45678"
+// #define TRACE_OUT_STREAM_TO_FILE "trace-out.log.cpp"
+// #define TRACE_OUT_STREAM_TO_WDO
+// #define TRACE_OUT_SYNC_STREAM // enable output syncronization
+// #define TRACE_OUT_SHOW_THREAD // show thread information
+// #define TRACE_OUT_STREAM_WIDTH 80 // width to which some output is wrapped
+
