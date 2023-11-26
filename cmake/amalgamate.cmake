2fca19c99f1f4b3468572cb0e7742f0189a90001
shrpnsld <anton.degtiar@gmail.com>
2023-11-26 23:51:51 +0200

    Added amalgamated header tests for C++98, C++17 and C++23

--- cmake/amalgamate.cmake
+++ cmake/amalgamate.cmake
@@ -1,4 +1,5 @@
 include(ExternalProject)
+include(get-property-default)
 
 ExternalProject_Add(amalgamate
 	GIT_REPOSITORY https://github.com/shrpnsld/amalgamate
@@ -22,12 +23,19 @@ set(AMALGAMATE_PATH ${SOURCE_DIR}/amalgamate)
 set(INTERLUDE_PATH ${PROJECT_SOURCE_DIR}/data/options-for-amalgamated-sources.hpp)
 set(FIRST_FILE trace-out/trace-out.hpp)
 get_target_property(TRACE_OUT_SOURCE_DIR trace-out INTERFACE_INCLUDE_DIRECTORIES)
+get_target_property_default(TRACE_OUT_INTERFACE_SOURCES trace-out INTERFACE_SOURCES "")
+get_target_property_default(TRACE_OUT_SOURCES trace-out SOURCES "")
 
 add_custom_target(trace-out.hpp
-	${AMALGAMATE_PATH} -i ${TRACE_OUT_SOURCE_DIR} -n trace-out -L ${INTERLUDE_PATH} -g -1 ${FIRST_FILE} -b 1 -t
-	COMMENT "Amalgamating sources..."
-	DEPENDS trace-out
+	DEPENDS ${PROJECT_BINARY_DIR}/trace-out-amalgamated/trace-out.hpp
+)
+
+add_custom_command(
+	OUTPUT ${PROJECT_BINARY_DIR}/trace-out-amalgamated/trace-out.hpp
+	COMMAND ${AMALGAMATE_PATH} -i ${TRACE_OUT_SOURCE_DIR} -n trace-out -L ${INTERLUDE_PATH} -g -1 ${FIRST_FILE} -b 1 -t -v
 	WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+	DEPENDS ${TRACE_OUT_INTERFACE_SOURCES} ${TRACE_OUT_SOURCES} ${INTERLUDE_PATH}
+	COMMENT "Amalgamating sources..."
 )
 
 add_dependencies(trace-out-amalgamated trace-out.hpp)
---
+++ tests/amalgamated-header-verification/amalgamated-header-verification-c++11.tests.cpp
@@ -0,0 +1,39 @@
+#include "dummy.hpp"
+#include "trace-out.hpp"
+#include <memory>
+
+int main()
+{$thread(main) $f
+	int subject1 {456};
+	float subject2 {789.123f};
+	std::string subject3 {"hellomoto!"};
+	std::unique_ptr<int> subject4 {new int {456}};
+
+	$t("hellomoto!");
+	$t(subject1, subject2, subject3, subject4);
+	$tr(subject3.begin(), subject3.end())
+	$m(subject3.c_str(), subject3.size(), $hex, $grp(1), $col(8))
+	$s(subject3 = "wazuuup!";)
+
+	$if (true)
+	{
+	}
+
+	$for (;;)
+	{
+		break;
+	}
+
+	$while(true)
+	{
+		break;
+	}
+
+	$time("dummy", dummy();)
+	$clocks("dummy", dummy();)
+	$time_stats("dummy", 1, dummy();)
+	$clock_stats("dummy", 1, dummy();)
+
+	$return 0;
+}
+
