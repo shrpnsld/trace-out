4a0cb27c81aa7733f9dfd4e2ad7b954a155acccf
shrpnsld <anton.degtiar@gmail.com>
2023-12-27 15:07:10 +0200

    Corrections and cosmetics

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,9 +1,11 @@
 include(GNUInstallDirs)
 
 install(CODE "execute_process(COMMAND cmake --build ${PROJECT_BINARY_DIR} --target trace-out.hpp)")
+get_target_property(AMALGAMATED_INCLUDE_DIR_PATH trace-out INTERFACE_INCLUDE_DIRECTORIES)
+set(TRACE_OUT_HPP_PATH "${AMALGAMATED_INCLUDE_DIR_PATH}/trace-out/trace-out.hpp")
 
 install(
-	FILES ${PROJECT_BINARY_DIR}/trace-out-amalgamated/trace-out.hpp
+	FILES ${TRACE_OUT_HPP_PATH}
 	DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
 )
 
