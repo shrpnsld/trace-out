05cda251f2e2255177176b7cf0d2e469f124e6a7
shrpnsld <anton.degtiar@gmail.com>
2024-02-14 22:30:32 +0200

    Added move semantics to 'resource'

--- src/trace-out/resource.hpp
+++ src/trace-out/resource.hpp
@@ -8,6 +8,11 @@
 namespace trace_out
 {
 
+struct move_tag
+{
+} MOVE_RESOURCE;
+
+
 template <typename Type_t>
 class resource
 {
@@ -15,13 +20,14 @@ public:
 	typedef void (*deleter_t)(Type_t);
 
 	resource(Type_t handle, deleter_t deleter);
+	resource(const resource &); // no implementation, so linker errors if copy constructor is being called
+	resource(const resource &another, move_tag);
 	~resource();
 
 	const Type_t &get() const;
 
 private:
 	resource();
-	resource(const resource &);
 	resource &operator =(const resource &);
 
 #if TRACE_OUT_CPP_VERSION >= 201103L
@@ -34,6 +40,17 @@ private:
 	Type_t _handle;
 	deleter_t _deleter;
 };
+
+}
+
+//
+// Private
+
+namespace trace_out
+{
+
+template <typename Type_t>
+Type_t move_fundamental(Type_t &from, Type_t zero = Type_t());
 
 }
 
@@ -52,15 +69,34 @@ resource<Type_t>::resource(Type_t handle, deleter_t deleter)
 }
 
 template <typename Type_t>
+resource<Type_t>::resource(const resource &another, move_tag)
+	:
+	_handle(move_fundamental(const_cast<resource<Type_t> &>(another)._handle)),
+	_deleter(move_fundamental(const_cast<resource<Type_t> &>(another)._deleter))
+{
+}
+
+template <typename Type_t>
 resource<Type_t>::~resource()
 {
-	_deleter(_handle);
+	if (_deleter != NULL)
+	{
+		_deleter(_handle);
+	}
 }
 
 template <typename Type_t>
 const Type_t &resource<Type_t>::get() const
 {
 	return _handle;
+}
+
+template <typename Type_t>
+Type_t move_fundamental(Type_t &from, Type_t zero)
+{
+	Type_t value = from;
+	from = zero;
+	return value;
 }
 
 }
