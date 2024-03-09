45fa81d46650c1bd6d4ebc0d83ce279125eaf19b
shrpnsld <anton.degtiar@gmail.com>
2024-03-09 13:07:44 +0200

    Fixed compilation error in 'stream-to-wdo-option.tests.cpp'

--- tests/stream-to-wdo-option/stream-to-wdo-option.tests.cpp
+++ tests/stream-to-wdo-option/stream-to-wdo-option.tests.cpp
@@ -21,7 +21,7 @@ int main()
 	$toct(subject2);
 	$thex(subject2);
 	$tr(subject3.begin(), subject3.end())
-	$m(subject3.c_str(), subject3.size(), $hex(1), $col(8))
+	$m(subject3.c_str(), subject3.size(), $hex, $grp(1), $col(8))
 	$s(subject3 = "wazuuup!";)
 
 	$if (true)
